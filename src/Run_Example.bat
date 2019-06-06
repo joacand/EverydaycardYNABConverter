e7e3be1a8a3baf95da22c126942b88ed9fe6743b
Joacim Andersson <joacand@users.noreply.github.com>
2019-06-06 09:51:52 +0200

    Update XLS file name in example to match reality

--- src/Run_Example.bat
+++ src/Run_Example.bat
@@ -1,3 +1,3 @@
 @echo off
-Powershell.exe -executionpolicy remotesigned -File ConvertXlsToYnabCsv.ps1 "kontotransactionlist.xls" "output_everydaycard.csv"
+Powershell.exe -executionpolicy remotesigned -File ConvertXlsToYnabCsv.ps1 "Transaktionslista.xls" "output_everydaycard.csv"
 pause
